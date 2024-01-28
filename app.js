c8f788a5cb90877bc7ba7dbcaadd422a2a854bc4
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-01-28 18:39:51 +0000

    MongoDB and Mongoose implementation

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect(
+    'mongodb+srv://jazibkhan:' + 
+    process.env.MONGO_ATLAS_PW + 
+    '@cluster0.mapzbsz.mongodb.net/?retryWrites=true&w=majority', 
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    }
+);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
